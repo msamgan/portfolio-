5fdcbc795688b3a35b4a7ad4e225adab172ab85b
msamgan <mohdsamgankhan@gmail.com>
2024-06-12 19:51:00 -0400

    added meta to posts page.

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -2,8 +2,6 @@
 
 import { useCallback, useEffect, useState } from "react"
 import axios from "axios"
-import Link from "next/link"
-import Image from "next/image"
 import Loading from "../loading"
 import PostList from "../components/PostList"
 
@@ -33,12 +31,27 @@ export default function Posts() {
 
     return (
         <div className="">
-            <title>Posts - msamgan.com</title>
+            <title>Published Posts - msamgan.com</title>
             <meta
                 name="description"
                 content="Explore msamgan's technical blog featuring insightful programming posts, in-depth code tutorials, and the latest updates in the tech world. Join our community and elevate your coding skills!"
             />
             <meta name="keywords" content="msamgan, blog, technical blog, programming, code, posts" />
+
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content="Published Posts - msamgan.com" />
+            <meta property="og:url" content="https://msamgan.com/posts" />
+            <meta property="og:description"
+                  content="Explore msamgan's technical blog featuring insightful programming posts, in-depth code tutorials, and the latest updates in the tech world. Join our community and elevate your coding skills!" />
+            <meta property="article:author" content="msamgan" />
+            <meta property="article:section" content="Published Posts" />
+            <meta property="article:tag" content="msamgan, blog, technical blog, programming, code, posts" />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="@msamgan" />
+            <meta name="twitter:title" content="Published Posts - msamgan.com" />
+            <meta name="twitter:description"
+                  content="Explore msamgan's technical blog featuring insightful programming posts, in-depth code tutorials, and the latest updates in the tech world. Join our community and elevate your coding skills!" />
+            <meta name="twitter:creator" content="@msamgan" />
 
             {loading ? (
                 <Loading />
