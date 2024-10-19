038239d6bb96d9b2a8db684d676ec0230725803e
msamgan <mohdsamgankhan@gmail.com>
2024-10-19 11:55:25 -0400

    page UI

--- src/app/page/[slug]/page.js
+++ src/app/page/[slug]/page.js
@@ -1,18 +1,11 @@
-import fs from "fs"
-import Markdown from "markdown-to-jsx"
 import "./page.css"
 
-async function getFileContent({ slug }) {
-    return fs.readFileSync("src/app/page/" + slug + ".md", "utf8")
-}
-
 export default async function Page(props) {
-    const slug = props.params.slug
-    const mdFile = await getFileContent({ slug })
+    /* const slug = props.params.slug*/
 
     return (
         <div className="space-y-12 page">
-            <Markdown>{mdFile}</Markdown>
+            In work
         </div>
     )
 }
